ed5146558098fd34683d311b6d718cd971829002
Keiya-Wada <wadakeiya@gmail.com>
2024-06-29 21:42:30 -0700

    Lab1 Submission

--- week1/js/init.js
+++ week1/js/init.js
@@ -1,14 +1,20 @@
 // Initialize the map
 const map = new maplibregl.Map({
     container: 'map', // container ID
-    style: 'https://api.maptiler.com/maps/streets/style.json?key=wsyYBQjqRwKnNsZrtci1', // Your style URL
-    center: [-118.444,34.0709], // Starting position [lng, lat]
-    zoom: 15 // Starting zoom level
+    style: 'https://api.maptiler.com/maps/streets/style.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL', // Your style URL
+    center: [-122.32270410130093, 37.56414483125629], // Starting position [lng, lat]
+    zoom: 13 // Starting zoom level
 });
 
 // Add a marker to the map
 new maplibregl.Marker()
-    .setLngLat([-118.444, 34.0709])
+    .setLngLat([-122.3296, 37.5508])
     .setPopup(new maplibregl.Popup({ offset: 25 }) // Add popups
-        .setHTML('Math Sciences 4328 aka the Technology Sandbox<br> is the lab where I used to work in '))
+        .setHTML('The high school that I currently attend '))
+    .addTo(map);
+
+    new maplibregl.Marker()
+    .setLngLat([-122.32311266123108, 37.56608627483537])
+    .setPopup(new maplibregl.Popup({ offset: 25 }) // Add popups
+        .setHTML('My favorite place to eat in my local downtown area'))
     .addTo(map);
